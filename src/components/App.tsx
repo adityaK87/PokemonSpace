8a983d848cefad75542290f8601a931c96c66332
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-22 11:00:47 +0530

    first Commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css'
+
+ class App  extends  React.Component  {
+    
+render() {
+    return (
+         <div>Pokedex</div>
+    );
+}
+    
+ }
+
+ export default App;
