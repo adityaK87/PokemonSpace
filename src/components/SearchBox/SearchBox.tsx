c7d0e2fb002b68d0c92d49fe74d5bffaf5d766cb
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-24 11:59:32 +0530

    add searchBox

---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SearchBox.css"
+
+const SearchBox = () =>{
+    return(
+        <input 
+        className="search  "
+        type="search" 
+        placeholder="search pokemons"
+        />
+
+    )
+}
+
+
+export default SearchBox;
