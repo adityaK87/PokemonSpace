8e8e9574af4a9b7934babe631e0f855457203ffe
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-30 18:55:11 +0530

    Comment added

--- src/components/PokeList/PokeList.tsx
+++ src/components/PokeList/PokeList.tsx
@@ -2,32 +2,33 @@ import React from "react";
 import { PokemonSchema } from "../../Types/PokemonSchema";
 import PokeCard from "../PokeCard/PokeCard";
 import "./PokeList.css"
- interface pokelistprops{
+
+interface pokelistprops{
     searchedPokemons:PokemonSchema[];
     onPokemonClick:(pokemonName:string)=>void;
  }
+
 const  PokeList = ({searchedPokemons,onPokemonClick}:pokelistprops) => {
-return(
+  return(
     <div
     className="pokelist">
         {
-            searchedPokemons.map((pokemon) =>{
+            searchedPokemons.map((pokemon) => {
+                // This map function is use to iterate every elements of an array
                 // console.log(pokemon)
                 return (
                     pokemon.name! &&(
                         <PokeCard 
-                        name={pokemon.name} 
+                         name={pokemon.name} 
                          key={pokemon.id} 
                          spriteUrl={pokemon.sprites?.normal}
                          onPokemonClick={onPokemonClick}
                          />
-
                     )
                 )
             })
         }
-    
     </div>
-)
-}
+)}
+
 export default PokeList;
