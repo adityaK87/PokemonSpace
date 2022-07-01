0dc11869f048e721921c8803a25d660e3e8d563a
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-01 11:08:38 +0530

    add footer

--- src/components/PokeSearchResult/PokeSearchResult.tsx
+++ src/components/PokeSearchResult/PokeSearchResult.tsx
@@ -1,27 +1,43 @@
 import React from "react";
+import { PokemonSchema } from "../../Types/PokemonSchema";
+import Footer from "../Footer/Footer";
 import "./PokeSearchResult.css"
-const PokeSearchResult = () => {
-    const selectedPokemon = false;
 
+
+interface pokeSearchResultprops{
+  selectedPokemon:PokemonSchema | undefined;
+}
+
+const PokeSearchResult = ({selectedPokemon}:pokeSearchResultprops) => {
+    const {name ,id ,height,weight,base_experience,sprites} = selectedPokemon || {}
     return  (
-        <div className="poke-result-card">
+        <div className="poke-container">       
+         <div className="poke-result-card">
             {
                 selectedPokemon 
                 ?
                 (
                     <div> 
                         {/* Add the image here */}
-                        <p>Name: Pikachu</p>
-                        <p>Id: some id</p>
-                        <p>Height: some Height</p>
-                        <p>weight: some weight</p>
-                        <p>Base Exp : 100</p>
+                        <img src={sprites?.animated || sprites?.normal} 
+                        alt="pokemon" 
+                        className="pokeSearchResult-img" />
+                        <p>Name:{name}</p>
+                        <p>Id:{id}</p>
+                        <p>Height:{height}</p>
+                        <p>Weight:{weight}</p>
+                        <p>Base Exp:{base_experience}</p>
                     </div>
                 ): (
                     <h2>Welcome to the Pokedex  </h2>
                 )
             }
         </div>
+        <div className="footer">
+            <Footer />
+        </div>
+        </div>
+
     )
 }
 
