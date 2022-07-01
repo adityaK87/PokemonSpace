467ccae4ec0b215760d489964e8aa9856ad0ce68
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-01 10:55:35 +0530

    Add Footer to pokemonSearchResult

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import  './Footer.css'
+const Footer = () => {
+    return(
+        <div className='footer-container'>
+            <a
+            target="_blank"
+            rel="noreferrer"
+            className='footer-link'
+             href="https://github.com/adityapal87/PokemonSpace">Get Code</a>
+
+        </div>
+    );
+}
+export default Footer;
